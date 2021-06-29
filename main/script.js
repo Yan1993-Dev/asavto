2195239aa4ddae688c5fff68d3badbdf3c5c5be2
Yan1993-Dev <66394095+Yan1993-Dev@users.noreply.github.com>
2021-06-29 15:36:05 +0300

    Слайдер заработал, осталось настроить

--- main/script.js
+++ main/script.js
@@ -1,20 +1,15 @@
-const wrapper = document.querySelector('slider_wrapper_js');
-const innerWrapper = wrapper.querySelector('.slider_inner_wrapper_js');
-const slides = innerWrapper.querySelector('.slides_slide_js');
-const buttonBack = document.querySelector('slider_button-back-js');
-const buttonNext = document.querySelector('slider_button-next_js');
+const slider = document.querySelector('.swiper-container');
 
-const timeAnimation = 500;
-const width = wrapper.clientWidth;
-const maxIndex = slides.lenght - 1;
-let activeIndex = 0;
-
-function setDisableButton(button, disable = true) {
-    if(disable) {
-        button.disable = 'disabled'
-    }
-    else {
-        button.addAttribute('disabled');
-    }
-}
-setDisableButton
+let mySwiper = new Swiper(slider, {
+	slidesPerView: 3,
+	spaceBetween: 10,
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+})
