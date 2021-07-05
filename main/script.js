8fe6ce7cbdd261dc8a92b1164eaad9797e6d1c48
Yan1993-Dev <66394095+Yan1993-Dev@users.noreply.github.com>
2021-07-05 14:31:11 +0300

    Верстка popup
    разработка попапа и другие мелочи

--- main/script.js
+++ main/script.js
@@ -118,3 +118,24 @@ document.addEventListener('DOMContentLoaded', () => {
 		});
 	});
 });
+
+
+// popup
+
+var popup = document.querySelector('.popup');
+var openPopup = document.querySelector('.open_popup_js')
+var overlay = document.querySelector('.popup_overlay')
+var popupCloseBtn = popup.querySelector('.popup_drive__btn')
+
+openPopup.addEventListener('click', function(){
+	popup.classList.add('popup_open');
+	overlay.classList.add('join');
+
+	popupCloseBtn.addEventListener('click', function(){
+		popup.classList.remove('popup_open');
+		overlay.classList.remove('join')
+	})
+
+})
+
+// календарь
